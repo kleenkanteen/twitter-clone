080c89952416b57053cb7c9d5d09e1b21fd0d5d7
kleenkanteen <antpalmolive@gmail.com>
2023-07-05 02:58:45 -0400

    not wokring pagination

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,5 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  
     <App />
-  
 );
--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -10,7 +10,8 @@ import {
   getDocs,
   query,
   where,
-  limit
+  limit,
+  startAfter
 } from "firebase/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
 import InfiniteScroll from 'react-infinite-scroller';
@@ -28,6 +29,7 @@ export interface Follow {
   followingUserId: string;
 }
 
+// home prop means if only subscribing to followed users
 export const Main = ({ home }: { home: boolean }) => {
   const [user, isLoading] = useAuthState(auth);
 
@@ -35,16 +37,25 @@ export const Main = ({ home }: { home: boolean }) => {
   const [followingList, setFollowingList] = useState<string[] | null>(null);
 
   // console.log(`goign home is ${home}`);
+
+  const postsRef = collection(db, "posts");
+  let postsQuery: any;
+  let snapshot: any;
+  let lastDocPagination: any;
   
   const getPosts = async () => {
-    const postsRef = collection(db, "posts");
-    let data: any;
     console.log(`current user is ${user?.displayName} ID is ${user?.uid}`)
 
     if (!home) {
       console.log("looking for all posts");
-      const postsQuery = query(postsRef, limit(10));
-      data = await getDocs(postsQuery);
+      if (!snapshot) {
+        postsQuery = query(postsRef, limit(2));
+      }
+      else {
+        postsQuery = query(postsRef, startAfter(lastDocPagination), limit(2));
+      }
+      snapshot = await getDocs(postsQuery);
+      lastDocPagination = snapshot.docs[snapshot.docs.length-1];
     } 
     else {
       if (!user) {
@@ -58,8 +69,8 @@ export const Main = ({ home }: { home: boolean }) => {
       const followingSnapshot: any = await getDocs(followerQuery);
 
       if (!followingSnapshot.empty) {
-      // console.log(`following userID ${followingData.docs[0].data().FollowingUserId}`);
-      const followingPromises = await followingSnapshot.docs.map(async (doc: any) => {
+      // console.log(`following userID ${followingsnapshot.docs[0].data().FollowingUserId}`);
+        const followingPromises = await followingSnapshot.docs.map(async (doc: any) => {
         console.log(`following userID ${doc.data().FollowingUserId}`);
         return doc.data().FollowingUserId;
       });
@@ -70,35 +81,59 @@ export const Main = ({ home }: { home: boolean }) => {
       console.log(`Following list is ${resolvedFollowingList}`);
       if (!resolvedFollowingList) return;
       const SubscribedPosts = query(postsRef, where("userId", "in", resolvedFollowingList));
-      data = await getDocs(SubscribedPosts);
+      snapshot = await getDocs(SubscribedPosts);
     }
     };
-  
-    setPostsList(
-      data?.docs.map((doc: any) => ({ ...doc.data(), id: doc.id })) as Post[]
-    );
+    
+    // if (!postsList) setPostsList([]);
+    // let newPosts: Post[] = snapshot?.docs.map((doc: any) => ({ ...doc.data(), id: doc.id })) as Post[];
+    // const updatePostsList = (prev: Post[]) => {
+    //   // Perform your logic to update the state variable
+    //   const addPosts = [...prev, newPosts];
+    //   return addPosts;
+    // };
+    
+    let newPosts: Post[] = snapshot?.docs.map((doc: any) => ({ ...doc.data(), id: doc.id })) as Post[]
+
+    if (!postsList) setPostsList(newPosts)
+
+    else {
+      setPostsList([...postsList, ...newPosts])
+    }
   };
+
+  const hasMorePosts = () => !snapshot.empty;
   
   useEffect(() => {
     if (user) getPosts();
   }, [home, user]);
 
+
   if (isLoading) {
-    return <Loading/>
-  }  
-  return (
-    <>
-      {!postsList && home ? (
-        <div className="color-scheme main full-height-border">
-          <div className="button-link height-border">You follow no one</div>
+    return <Loading />
+  } 
+  if (!postsList && home) {
+    return (
+      <div className="color-scheme main full-height-border">
+        <div className="button-link height-border">
+          You follow no one
         </div>
-      ) : null}
-      <div>
-        {postsList?.map((post) => (
-          <Post post={post} key={post.id} postsList={postsList} setPostsList={setPostsList} />
-        ))
-        }
       </div>
-    </>
+    )
+  }
+  return (
+    <div className="posts full-height-border">
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={getPosts}
+        hasMore={snapshot ? hasMorePosts() : false}
+        loader={<Loading />}
+      >
+          {postsList?.map((post) => (
+            <Post post={post} key={post.id} postsList={postsList} setPostsList={setPostsList} />
+          ))
+          }
+      </InfiniteScroll>
+    </div>
   );
 }
